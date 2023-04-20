c5e782cc52afdfbe79de15848f0d70a370ee7c76
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-04-21 00:47:40 +0200

    [Perf] Demo: ChangeDetection

--- src/app/flight-booking/flight-card/flight-card.component.ts
+++ src/app/flight-booking/flight-card/flight-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, input, NgZone, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 import { Flight } from '../../entities/flight';
@@ -10,12 +10,15 @@ import { Flight } from '../../entities/flight';
   styleUrls: ['./flight-card.component.css'],
 })
 export class FlightCardComponent implements OnInit, OnChanges, OnDestroy {
-  private debug = false;
+  private debug = true;
 
   // @Input({required: true}) item!: Flight;
   readonly item = input.required<Flight>();
   // @Input() selected = false;
   readonly selected = input(false);
+
+  private readonly elementRef = inject(ElementRef);
+  private readonly ngZone = inject(NgZone);
 
   constructor() {
     this.debugInputs('constructor');
@@ -39,5 +42,18 @@ export class FlightCardComponent implements OnInit, OnChanges, OnDestroy {
       console.debug('flight', this.item);
       console.debug('selected', this.selected);
     }
+  }
+
+  blink(): void {
+    // Dirty Hack used to visualize the change detector
+    // let originalColor = this.elementRef.nativeElement.firstChild.style.backgroundColor;
+    this.elementRef.nativeElement.firstChild.style.backgroundColor = 'crimson';
+    //              ^----- DOM-Element
+
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        this.elementRef.nativeElement.firstChild.style.backgroundColor = this.selected() ? 'rgb(204, 197, 185)' : '';
+      }, 1000);
+    });
   }
 }
