0ca4de6d5aedeb040456240980ef0e162653f010
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-04-01 14:31:30 +0200

    Add RxJS operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { FlightLookaheadComponent } from "./flight-lookahead/flight-lookahead.component";
 
 @Component({
-  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent],
+  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent, FlightLookaheadComponent],
   selector: 'app-flight-app',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
