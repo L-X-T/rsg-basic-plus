L-X-T/sybit-essentials-modern

Add AirportsComponent

src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { AirportsComponent } from './airports/airports.component';
 
 @Component({
   standalone: true,
-  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent],
+  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent, AirportsComponent],
   selector: 'app-flight-app',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',

L-X-T/rsg-basic-plus

src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FlightSearchComponent } from './flight-search/flight-search.component';
-import { FlightLookaheadComponent } from "./flight-lookahead/flight-lookahead.component";
+import { AirportsComponent } from './airports/airports.component';
+import { FlightLookaheadComponent } from './flight-lookahead/flight-lookahead.component';
 
 @Component({
-  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent, FlightLookaheadComponent],
+  imports: [SidebarComponent, NavbarComponent, AirportsComponent, FlightLookaheadComponent],
   selector: 'app-flight-app',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',