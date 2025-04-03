a4a747e3c14fe5d08c09dd3b6dcc0fb0aca2aa2e
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-04-03 14:56:53 +0200

    [UT] Preparation

--- src/app/flight-booking/shared/validation/round-trip-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoundTripValidatorDirective } from './round-trip-validator.directive';
-
-describe('RoundTripValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RoundTripValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
